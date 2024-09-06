23e1307e22649349115dd1ee0008555761d0635c
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-06 18:36:10 -0300

    Fixed lines

--- Menu/Menu/Menu.gd
+++ Menu/Menu/Menu.gd
@@ -1,9 +1,5 @@
 extends Control
 
-func _ready():
-	$Button2Players.connect("pressed", Callable(self, "_on_Button2Players_pressed"))
-	$Button3Players.connect("pressed", Callable(self, "_on_Button3Players_pressed"))
-
 func start_game(player_count: int):
 	queue_free() 
 	var game_manager = preload("res://Director/GameManager/GameManager.tscn").instantiate()
