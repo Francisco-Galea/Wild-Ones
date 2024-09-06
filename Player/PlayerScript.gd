8a2ab6282f0f82ca6457b369d74afbf6724bf396
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-06 12:23:36 -0300

    Added turn manager

--- Player/PlayerScript.gd
+++ Player/PlayerScript.gd
@@ -1,29 +1,28 @@
 extends CharacterBody2D
 
 var velocidad: int = 500
-var _gravity: float
+var gravity: float
 var projectile_scene: PackedScene = preload("res://Weapon/Projectile.tscn")
-var is_turn: bool = false  # Indica si es el turno del personaje
+var is_turn: bool = false  
+
 
 func _ready():
-	_gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-	set_physics_process(true)  # Siempre procesar física
+	gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+	set_physics_process(true) 
 
 func _physics_process(delta):
-	# Aplicar gravedad siempre
+	
 	if not is_on_floor():
-		velocity.y += _gravity * delta
+		velocity.y += gravity * delta
 	else:
 		velocity.y = 0
-
-	# Procesar movimiento solo si es el turno del jugador
+	
 	if is_turn:
-		_handle_movement()
-
-	# Mover al personaje
+		handle_movement()
+	
 	move_and_slide()
 
-func _handle_movement():
+func handle_movement():
 	var input_direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	velocity.x = input_direction * velocidad
 
@@ -31,20 +30,16 @@ func _handle_movement():
 		velocity.y = -velocidad
 
 	if Input.is_action_just_pressed("shoot"):
-		_shoot_projectile()
+		shoot_projectile()
 
-func _shoot_projectile():
+func shoot_projectile():
 	var projectile = projectile_scene.instantiate()
 	projectile.position = position
 	var mouse_position = get_global_mouse_position()
-
-	# Dirección del proyectil
 	var direction = (mouse_position - global_position).normalized()
 	projectile.position = position + direction * 100
-
-	# Velocidad del proyectil
 	projectile.velocity = direction * 900
 	get_parent().add_child(projectile)
 
 func set_turn(turn: bool):
-	is_turn = turn  # Cambiar el turno del personaje
+	is_turn = turn 
