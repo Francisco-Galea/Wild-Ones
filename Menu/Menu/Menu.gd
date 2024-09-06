da146dee6c05428cad2e4d10ee9a857e3d042ad8
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-06 12:22:19 -0300

    Clean code

--- Menu/Menu/Menu.gd
+++ Menu/Menu/Menu.gd
@@ -4,14 +4,14 @@ func _ready():
 	$Button2Players.connect("pressed", Callable(self, "_on_Button2Players_pressed"))
 	$Button3Players.connect("pressed", Callable(self, "_on_Button3Players_pressed"))
 
-func _start_game(player_count: int):
+func start_game(player_count: int):
 	queue_free() 
 	var game_manager = preload("res://Director/GameManager/GameManager.tscn").instantiate()
 	game_manager.set_player_count(player_count)
 	get_tree().root.add_child(game_manager)
 
 func _on_Button2Players_pressed():
-	_start_game(2)
+	start_game(2)
 
 func _on_Button3Players_pressed():
-	_start_game(3)
+	start_game(3)
