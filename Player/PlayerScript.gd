70645574e8b5cd4e27fbeb2171420ddd1ed61f1d
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-06 21:24:32 -0300

    Added Area2d for

--- Player/PlayerScript.gd
+++ Player/PlayerScript.gd
@@ -5,7 +5,6 @@ var gravity: float
 var projectile_scene: PackedScene = preload("res://Weapon/Projectile.tscn")
 var is_turn: bool = false  
 
-
 func _ready():
 	gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 	set_physics_process(true) 
@@ -26,10 +25,10 @@ func _physics_process(delta):
 func handle_movement():
 	var input_direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	velocity.x = input_direction * velocidad
-
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -velocidad
-
+	
 	if Input.is_action_just_pressed("shoot"):
 		shoot_projectile()
 
@@ -44,3 +43,8 @@ func shoot_projectile():
 
 func set_turn(turn: bool):
 	is_turn = turn 
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("bullets"):
+		body.queue_free()
