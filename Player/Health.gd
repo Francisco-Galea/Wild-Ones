5264c4acf48b0965c5f59f311cd20adae0706dba
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-27 23:28:38 -0300

    Added component health

---
+++ Player/Health.gd
@@ -0,0 +1,23 @@
+extends Node
+
+signal health_changed(new_health)
+signal died
+
+@export var max_health: int = 100
+var current_health: int
+
+func _ready():
+	current_health = max_health
+
+func take_damage(amount: int):
+	current_health = max(0, current_health - amount)
+	emit_signal("health_changed", current_health)
+	if current_health == 0:
+		emit_signal("died")
+
+func heal(amount: int):
+	current_health = min(max_health, current_health + amount)
+	emit_signal("health_changed", current_health)
+
+func get_health():
+	return current_health
