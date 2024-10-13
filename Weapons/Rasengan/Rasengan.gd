ff2987eb2f5a0c7817cbf3913770be8aa6769be7
Francisco Galea <galeafrancisco2001@gmail.com>
2024-10-13 02:04:25 -0300

    Rasengan destroy terrain

--- Weapons/Rasengan/Rasengan.gd
+++ Weapons/Rasengan/Rasengan.gd
@@ -1,14 +1,22 @@
 extends RigidBody2D
 
 var damage_rasengan
-
-func _ready():
-	add_to_group("projectiles")
+var destruction_radius: int = 2
+@onready var sound = $AudioStreamPlayer
 
 func _on_body_entered(body):
 	if body.has_method("take_damage"):
 		body.take_damage(damage_rasengan)
+	if body is TileMap:
+		destroy_terrain(body, global_position)
 	queue_free()
+
+func destroy_terrain(tilemap: TileMap, impact_position: Vector2):
+	for x in range(-destruction_radius, destruction_radius + 1):
+		for y in range(-destruction_radius, destruction_radius + 1):
+			var tile_pos = tilemap.local_to_map(tilemap.to_local(impact_position))
+			tile_pos += Vector2i(x, y)
+			tilemap.set_cell(0, tile_pos, -1)  
 
 func set_damage(damage: int):
 	damage_rasengan = damage
