d5a6ea4d87c9a0b800a0a149f3df8565eab79712
Francisco Galea <galeafrancisco2001@gmail.com>
2024-10-17 14:30:53 -0300

    Implemented animation swing to Light saber

--- Weapons/Lightsaber/Lightsaber.gd
+++ Weapons/Lightsaber/Lightsaber.gd
@@ -1,13 +1,18 @@
 extends Area2D
 
 var cut_damage: int 
+@onready var swing_animation = $Sprite2D/SwingAnimation
 
+func _ready():
+	swing_animation.play("swing")
+	await swing_animation.animation_finished
+	queue_free()
 
 func _on_body_entered(body):
+	swing_animation.play("swing")
 	print("Estoy dentro de sable laser")
 	if body.has_method("take_damage"):
 		body.take_damage(cut_damage)
-	queue_free()
 
 func set_damage(damage: int):
 	cut_damage = damage
